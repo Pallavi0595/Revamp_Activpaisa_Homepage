cf87cf10fbd09695c2f762dd49668d8916b5b291
pallavi-activPaisa <pallavi@actipaisa.com>
2025-12-11 12:52:59 +0530

    partner page first commit

--- src/app/Channel-partners/page.jsx
+++ src/app/Channel-partners/page.jsx
@@ -1,7 +1,17 @@
+// import PartnerNavbar from "@/channel-partners/components/Navbar/PartnerNavbar";
+import PartnerNavbar from "@/channel-partners/components/Navbar/PartnerNavbar";
+// import Navbar from "@/components/Navbar/Navbar";
+import BecomeActivPaisaLoan from "@/channel-partners/components/HeroSection/BecomeActivPaisaLoan";
+import WhoLoanAdvisor from "@/channel-partners/components/WhoLoanAdvisor/WhoLoanAdvisor";
+import ExploreRangeCreditProducts from "@/channel-partners/components/ExploreRangeCreditProducts/ExploreRangeCreditProducts";
 export default function ChannelPartners() {
   return (
-    <div style={{ padding: "40px" }}>
-      <h1>Welcome to Channel Partners Page</h1>
+    <div>
+      <PartnerNavbar />
+      <BecomeActivPaisaLoan />
+      <WhoLoanAdvisor />
+      <ExploreRangeCreditProducts />
+      {/* <h1>Welcome to Channel Partners Page</h1> */}
     </div>
   );
 }

14715d78e984d7d9511e41412928b74146fd406d
sultan-activ <sultan@activpaisa.com>
2025-12-11 10:37:52 +0530

    some section of partner page added

--- src/app/Channel-partners/page.jsx
+++ src/app/Channel-partners/page.jsx
@@ -1,7 +1,23 @@
+import BecomeAdvisor from "@/channel-partners/components/become-advisor/BecomeAdvisor";
+import JoinUs from "@/channel-partners/components/join-us/JoinUs";
+import LenderScroll from "@/channel-partners/components/lenders-scroll/LenderScroll";
+import PartnersReviews from "@/channel-partners/components/partners-reviews/PartnersReviews";
+import StillHaveQuestion from "@/channel-partners/components/still-have-question/StillHaveQuestion";
+import ToolsAndSupport from "@/channel-partners/components/tools-and-support-provided/ToolsAndSupport";
+import WhyActivpaisa from "@/channel-partners/components/why-activpaisa/WhyActivpaisa";
+import Navbar from "@/components/Navbar/Navbar";
+
 export default function ChannelPartners() {
   return (
-    <div style={{ padding: "40px" }}>
-      <h1>Welcome to Channel Partners Page</h1>
+    <div className="w-full flex flex-col justify-center items-center ">
+      <Navbar />
+      <LenderScroll />
+      <WhyActivpaisa />
+      <BecomeAdvisor />
+      <ToolsAndSupport />
+      <JoinUs />
+      <PartnersReviews />
+      <StillHaveQuestion />
     </div>
   );
 }
